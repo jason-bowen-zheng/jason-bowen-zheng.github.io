615c7c5f7b193e7a95b70614aa0bd99f46b514ee
jason-bowen-zheng <18917572736@wo.cn>
2022-01-08 19:20:17 +0800

    2022-01-08

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,4 +1,4 @@
-mathsTips = [
+const mathsTips = [
 	"幂函数系数必为1，与坐标轴不交。",
 	"用指对互换解指对方程。",
 	"复合函数法求值域可不考虑函数天然定义域。",
@@ -17,11 +17,12 @@ mathsTips = [
 	"等式恒成立：标准化叙述，分清参变，化为<b>f(x)=0</b>，将所有变量归类合并得系数，系数全为零。"
 ];
 
-saying = [
+const quotes = [
 	["史家之绝唱，无韵之离骚。", "鲁迅"],
 	["我向往自由，我要谈恋爱！", "逍遥散人"],
 	["我亦无他，惟手熟尔。", "宋&middot;欧阳修"],
 	["生存，还是毁灭，这是个问题。", "莎士比亚"],
+	["人生如梦，一樽还酹江月。", "宋&middot;苏轼"],
 	["无边落木萧萧下，不尽长江滚滚来。", "唐&middot;杜甫"],
 	["不识庐山真面目，只缘身在此山中。", "宋&middot;苏轼"],
 	["长风破浪会有时，直挂云帆济沧海。", "唐&middot;李白"],
@@ -35,52 +36,80 @@ saying = [
 	["安得广厦千万间，大庇天下寒士俱欢颜，风雨不动安如山。", "唐&middot;杜甫"]
 ];
 
-images = [];
-$.ajax({
-	"url": "sources/images/lists.json",
-	"success": (data) => {
-		images = data;
-	}
-});
-
 function checkUpload() {
 	if (location.hostname.indexOf(".github.io") == -1) {
-		$("#submit-upload").attr("disabled", "1");
+		$("#submit-upload").attr("disabled", "disabled");
 	}
+}
+
+function getArticleFileName(year, month, day) {
+	month = (month.toString().length == 1)? `0${month}`: month.toString();
+	day = (day.toString().length == 1)? `0${day}`: day.toString();
+	return `articles/${year}-${month}-${day}.md`;
+}
+
+function onArticlesList(callback) {
+	$.ajax({
+		"url": "articles/lists.json",
+		"success": (list) => {
+			callback(list);
+		}
+	});
 }
 
 function showDailyImage() {
-	now = new Date();
-	index = (now.getDate() + now.getHours()) % images.length;
-	$("#daily-image").attr("src", `sources/images/${images[index].name}`);
-	$("#img-desp").html(images[index].description);
+	let now = new Date();
+	$.ajax({
+		"url": "sources/images/lists.json",
+		"success": (list) => {
+			let index = (now.getDate() + now.getHours()) % list.length;
+			$("#daily-image").attr("src", `sources/images/${list[index].name}`);
+			$("#img-desp").html(list[index].description);
+			$("#daily-image").attr("alt", list[index].description);
+		}
+	});
+}
+
+function showLatestArticle(articlesList) {
+	index = articlesList.length - 1;
+	$("#article-title").text(articlesList[index][1]);
+	$.ajax({
+		"url": getArticleFileName(...articlesList[index][0]),
+		"error": (xhr) => {
+			$("#article-content").html(`<span class="text-muted">文件未成功读取，错误代码：${xhr.status}。</span>`);
+		},
+		"success": (text) => {
+			result = marked.parse(text);
+			$("#article-content").html(result.slice(0, result.indexOf("</p>") + 4));
+		}
+	});
 }
 
 function showMathsTips() {
-	now = new Date();
-	index = (now.getDate() + now.getHours()) % mathsTips.length;
+	let now = new Date();
+	let index = (now.getDate() + now.getHours()) % mathsTips.length;
 	$("#maths-tips").html(mathsTips[index]);
 }
 
-function showSaying() {
-	now = new Date();
-	index = (now.getDate() + now.getHours()) % saying.length;
+function showQuote() {
+	let now = new Date();
+	let index = (now.getDate() + now.getHours()) % quotes.length;
 	if (now.getMonth() == 0 && now.getDate() == 1 && now.getHours() == 0) {
-		$("#saying").html(`<b>宋&middot;王安石</b>：爆竹声中一岁除，春风送暖入屠苏。`);
+		$("#quote").html(`<b>宋&middot;王安石</b>：爆竹声中一岁除，春风送暖入屠苏。`);
 	} else if (now.getMonth() == 1 && now.getDate() == 14) {
-		$("#saying").html(`<b>乔治&middot;艾略特</b>：我不但喜欢被人爱，还喜欢有人告诉爱上了我。`);
+		$("#quote").html(`<b>乔治&middot;艾略特</b>：我不但喜欢被人爱，还喜欢有人告诉爱上了我。`);
 	} else {
-		$("#saying").html(`<b>${saying[index][1]}</b>：${saying[index][0]}`);
+		$("#quote").html(`<b>${quotes[index][1]}</b>：${quotes[index][0]}`);
 	}
 }
 
 function showToday() {
-	now = new Date();
-	num2name = ["日", "一", "二", "三", "四", "五", "六"];
+	let now = new Date();
+	let num2name = ["日", "一", "二", "三", "四", "五", "六"];
 	$("#today").html(`今天是${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日`);
 	$("#today").html($("#today").html() + `（星期${num2name[now.getDay()]}），`);
-	thisYear = new Date(now.getFullYear(), 0);
-	nextYear = new Date(now.getFullYear() + 1, 0);
+	let thisYear = new Date(now.getFullYear(), 0);
+	let nextYear = new Date(now.getFullYear() + 1, 0);
 	if (thisYear - now < nextYear - now) {
 		$("#today").html($("#today").html() + `是今年的第${parseInt((now - thisYear) / 86400000 + 1)}天。`);
 	} else {
