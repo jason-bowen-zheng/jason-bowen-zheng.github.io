aea258485dcd6e6e4f2ea96defaecde08eb47aeb
jason-bowen-zheng <18917572736@wo.cn>
2022-05-17 10:45:20 +0800

    2022-05-17

---
+++ update-cache.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+
+function getBlogFileName(year, month, day) {
+    month = (month.toString().length == 1) ? `0${month}` : month.toString()
+    day = (day.toString().length == 1) ? `0${day}` : day.toString()
+    return `blogs/${year}-${month}-${day}.md`
+}
+
+let cache = []
+for (let blog of JSON.parse(fs.readFileSync("blogs/index.json"))) {
+    text = fs.readFileSync(getBlogFileName(...blog.time)).toString()
+    text = text.replace(/^#{1,6}\s*/gm, "").replace(/<!\-\-.+?\-\->/gsm, "")
+        .replace(/\$\$([^\$\$]+?)\$\$/g, "").replace(/\$([^\$]+?)\$/g, "")
+        .replace(/\s*-\s*/gm, "").replace(/\s*\d+\.\s*/gm, "")
+        .replace(/\s*>*\s*/gm, "").replace(/\*\*\*?/g, "").replace(/__/g, "")
+        .replace(/\[(.+?)\]\(.+?\)/g, "$1").replace(/&.+?;/g, "")
+        .replace(/[|:-]/g, "").replace(/\\/g, "").replace(/['"，。、；：？（）‘’“”]/g, "")
+    cache.push({
+        time: blog.time,
+        title: blog.title,
+        tags: blog.tags,
+        content: text
+    })
+}
+fs.writeFileSync("blogs/.cache.json", JSON.stringify(cache))
--- update-cache.py
+++
@@ -1,9 +0,0 @@
-import json
-from os import path
-
-def update_cache() :
-    for blog in json.load(open(path.join("blogs", "index.json"), "r+", encoding="utf-8")):
-        print(blog)
-
-if __name__ == "__main__":
-    update_cache()
